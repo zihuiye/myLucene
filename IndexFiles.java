960145580b1aea0f1a3b5fc527d2cad56224e5de
zihuiye <zihuiye@gmail.com>
2016-04-28 21:20:07 +0800

    boost

--- IndexFiles.java
+++ IndexFiles.java
@@ -56,7 +56,7 @@ import java.util.Map;
 public class IndexFiles {
   
 	protected static float anchorBoost = 1.5f;
-	protected static float titleBoost = 10f;
+	protected static float titleBoost = 2f;
 	
 	
 	
